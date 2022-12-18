aac793b095e7b9ca4101d225c661e62762f4d0cd
faslim <100623546+faslimpachu@users.noreply.github.com>
2022-12-18 13:23:06 +0530

    Add files via upload

--- file.js
+++ file.js
@@ -1,8 +1,29 @@
-$(document).ready(function(){
-  $("#nav-check1").click(function(){
-    $("#all-hide").hide();
- });
- $("#nav-check1").mouseleave(function(){
-   $("#all-hide").show();
- });
-});
+// $(document).ready(function(){
+//   $("#nav-check1").click(function(){
+//     $("#all-hide").hide();
+//  });
+//  $("#nav-check1").change(function(){
+//   $("#all-hide").show();
+// });
+
+
+// });
+// // $("#nav-check1").toggle(
+// //   function(){
+// //     $("#all-hide").children().show();
+// //   },
+// //   function(){
+// //     $("#all-hide").children().hide();
+// //   }
+
+// // );
+
+
+function myfun(){
+  var x = document.getElementById("#nav-check1");
+  if (x.style.display === "none"){
+    x.style.display = "block";
+  } else{
+    x.style.display ="none";
+  }
+}
